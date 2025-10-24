a982018af2983da565697ccd44cf639f68c7d4aa
amireal98 <amireal98@proton.me>
2025-10-24 11:07:19 -0600

    first commit

---
+++ configuration.nix
@@ -0,0 +1,72 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-btw";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Mexico_City";
+
+  programs.hyprland = {
+   enable = true;
+   xwayland.enable = true;
+  };
+
+  services.displayManager.ly.enable = true;
+
+  services.libinput.enable = true;
+
+  users.users.amireal = {
+   isNormalUser = true;
+   extraGroups = [ "wheel" ];
+   packages = with pkgs; [
+    tree
+   ];
+  };
+
+  programs.git = {
+   enable = true;
+   config = {
+    user.name = "amireal98";
+    user.email = "amireal98@proton.me";
+    init.defaultBranch = "main";
+   };
+  };
+  
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+   lazygit
+   nitch
+   fastfetch
+   kitty
+   waybar
+   swww
+   nautilus
+   nwg-look
+   neovim
+   rofi-wayland
+   brightnessctl
+   adwaita-icon-theme
+   gemini-cli
+   ollama
+   vscodium
+  ];
+
+  fonts.packages = with pkgs; [
+   nerd-fonts.jetbrains-mono
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "25.05"; # DONT TOUCH
+
+}
+
---
+++ home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+let
+ dotfiles = "${config.home.homeDirectory}/nixos-dots/config";
+ create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+ configs = {
+  hypr = "hypr";
+  kitty = "kitty";
+ };
+in
+
+{
+ home.username = "amireal";
+ home.homeDirectory = "/home/amireal";
+ programs.gh.enable = true;
+ programs.git.enable = true;
+ home.stateVersion = "25.05";
+ programs.bash = {
+  enable = true;
+  shellAliases = {
+   btw = "echo I use NixOS, btw";
+   nrs = "sudo nixos-rebuild switch --flake ~/nixos-dots#nixos-btw";
+   l = "ls -la";
+  };
+ };
+
+ xdg.configFile = builtins.mapAttrs
+  (name: subpath: {
+   source = create_symlink "${dotfiles}/${subpath}";
+   recursive = true;
+  }) configs;
+
+}
+
