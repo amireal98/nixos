28ab80dd4f6701c835ffb3c3d218c44cc802def4
amireal98 <amireal98@proton.me>
2025-10-25 12:41:37 -0600

    deleted nvf

--- flake.nix
+++ flake.nix
@@ -1,19 +1,17 @@
 {
  description = "NixOS";
  inputs = {
-  nvf.url = "github:notashelf/nvf";
   nixpkgs.url = "nixpkgs/nixos-25.05";
   home-manager = {
    url = "github:nix-community/home-manager/release-25.05";
    inputs.nixpkgs.follows = "nixpkgs";
   };
  };
- outputs = { self, nixpkgs, home-manager, nvf, ... }: {
+ outputs = { self, nixpkgs, home-manager, ... }: {
   nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
    system = "x86_64-linux";
    modules = [
     ./configuration.nix
-    nvf.homeManagerModules.default
     home-manager.nixosModules.home-manager
     {
      home-manager = {
