bc2a00566fd6a572e934aa4bef0fbed66c4d669f
amireal98 <amireal98@proton.me>
2025-10-25 13:01:09 -0600

    nvf 3 a1

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
+
  description = "NixOS";
+
  inputs = {
   nixpkgs.url = "nixpkgs/nixos-25.05";
+  nvf.url = "github:notashelf/nvf";
   home-manager = {
    url = "github:nix-community/home-manager/release-25.05";
    inputs.nixpkgs.follows = "nixpkgs";
   };
  };
- outputs = { self, nixpkgs, home-manager, ... }: {
+
+ outputs = { self, nixpkgs, home-manager, nvf, ... }: {
+
   nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
    system = "x86_64-linux";
    modules = [
     ./configuration.nix
+    nvf.nixosModules.default
     home-manager.nixosModules.home-manager
     {
      home-manager = {
@@ -23,6 +29,8 @@
     }
    ];
   };
+
  };
+
 }
 
