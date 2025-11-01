34c8971e8a01b73c6cf3a176686edbc36afdd939
amireal98 <amireal98@proton.me>
2025-11-01 17:10:58 -0600

    added stylix

--- flake.nix
+++ flake.nix
@@ -8,14 +8,16 @@
    url = "github:nix-community/home-manager";
    inputs.nixpkgs.follows = "nixpkgs";
   };
+  stylix.url = "github:danth/stylix",
  };
 
- outputs = { self, nixpkgs, home-manager, ... }: {
+ outputs = { self, nixpkgs, home-manager, stylix, ... }: {
 
   nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
    system = "x86_64-linux";
    modules = [
     ./configuration.nix
+    inputs.stylix.nixosModules.stylix
     home-manager.nixosModules.home-manager
     {
      home-manager = {
