4a65302274f9c7e51c1d67de83f9f79daacfec00
amireal98 <amireal98@proton.me>
2025-11-08 17:24:55 -0600

    changed to unstable branch

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
  description = "NixOS";
  inputs = {
-  nixpkgs.url = "nixpkgs/nixos-25.05";
+  nixpkgs.url = "nixpkgs/nixos-unstable";
   home-manager = {
-   url = "github:nix-community/home-manager/release-25.05";
+   url = "github:nix-community/home-manager";
    inputs.nixpkgs.follows = "nixpkgs";
   };
  };
