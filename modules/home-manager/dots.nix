75a622a6c2ee2deca16276371a7dc6ce21c563a8
amireal98 <amireal98@proton.me>
2025-11-09 16:53:28 -0600

    minor changes

--- modules/home-manager/dots.nix
+++ modules/home-manager/dots.nix
@@ -5,20 +5,20 @@
     ... 
 }:
 
-# Dotfiles and configs
+# Dotfiles
 let
- dotfiles = "${config.home.homeDirectory}/Nixos/config";
- create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
- configs = {
-  git = "git";
-  niri = "niri";
- };
+  dotfiles = "${config.home.homeDirectory}/Nixos/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    git = "git";
+    niri = "niri";
+  };
 in
 
 {
     xdg.configFile = builtins.mapAttrs
-   (name: subpath: {
-    source = create_symlink "${dotfiles}/${subpath}";
-    recursive = true;
-   }) configs;
+    (name: subpath: {
+        source = create_symlink "${dotfiles}/${subpath}";
+        recursive = true;
+    }) configs;
 }
