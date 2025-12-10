dea8bbd1e28e534b020717fb032d0e11ed3e1996
amireal98 <amireal98@proton.me>
2025-12-10 12:41:33 -0600

    adden locales

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
 
   time.timeZone = "America/Mexico_City";
 
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
