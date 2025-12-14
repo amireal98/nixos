22b6580e70ae3dea1d1d814db0065f217e1fcc4a
amireal98 <amireal98@proton.me>
2025-12-14 12:16:39 -0600

    minor bug

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;
     image = ../wallpapers/0008.png;
-    base24Scheme = "${pkgs.base24-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base24-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
     fonts = {
       monospace = {
