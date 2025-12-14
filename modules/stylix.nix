bc16b18677aa2d30ad007f8c969b28b75bb942bd
amireal98 <amireal98@proton.me>
2025-12-14 13:51:03 -0600

    tokyo night night

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;
     image = ../wallpapers/0008.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-moon.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     polarity = "dark";
     fonts = {
       monospace = {
