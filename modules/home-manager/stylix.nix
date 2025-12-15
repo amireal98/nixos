5efb957be4fa2c568ffd6aa83b10d9e38e5cf061
amireal98 <amireal98@proton.me>
2025-12-15 16:32:17 -0600

    idk

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -3,33 +3,10 @@
 {
   stylix = {
     enable = true;
-    image = ../../wallpapers/dark-lines.jpeg;
-
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/black-metal.yaml";
-    polarity = "dark";
-
-    fonts = {
-      monospace = {
-	package = pkgs.nerd-fonts.iosevka;
-	name = "Iosevka Nerd Font";
-      };
-      serif = {
-	package = pkgs.poppins;
-	name = "Poppins";
-      };
-      sansSerif = {
-	package = pkgs.poppins;
-	name = "Poppins";
-      };
-      emoji = {
-	package = pkgs.noto-fonts-color-emoji;
-	name = "Noto Color Emoji";
-      };
-    };
     
     targets = {
-      qt.enable = true;
       gtk.enable = true;
+      qt.enable = true;
       vicinae.enable = true;
       obsidian.enable = true;
       zen-browser = {
