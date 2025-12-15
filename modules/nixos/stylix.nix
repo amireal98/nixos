a2715e801577772f195d62d56ba26b5325dcd30b
amireal98 <amireal98@proton.me>
2025-12-15 17:17:37 -0600

    more testing

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;
     image = ../../wallpapers/dark-lines.jpeg;
-    autoEnable = false;
+    autoEnable = true;
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/black-metal.yaml";
     polarity = "dark";
