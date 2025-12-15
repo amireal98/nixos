302e2a36ae9a45b484dd5853d8da43b7b9e5938c
amireal98 <amireal98@proton.me>
2025-12-15 16:36:18 -0600

    added vscode to stylix targets

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -7,6 +7,7 @@
       qt.enable = true;
       vicinae.enable = true;
       obsidian.enable = true;
+      vscode.enable = true;
       zen-browser = {
 	enable = false;
 	profileNames = [ "default" "amireal" ];
