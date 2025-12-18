e5556658ddcc9a8a872d78490baf5ba92b415f6a
amireal98 <amireal98@proton.me>
2025-12-18 17:06:31 -0600

    added dwm

---
+++ suckless/suckless.nix
@@ -0,0 +1,17 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+
+    windowManager.dwm = {
+      enable = true;
+      package = pkgs.dwm.overrideAttrs {
+	scr = ./dwm;
+      };
+    };
+
+  };
+}
