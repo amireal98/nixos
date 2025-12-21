a56dd607b767413c19c877d72e3fba147e4aec04
amireal98 <amireal98@proton.me>
2025-12-21 12:39:15 -0600

    modules/nixos/ added prism launcher

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -1,49 +1,50 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowBroken = true;
-    permittedInsecurePackages = [
-      "mbedtls-2"
-      "mbedtls-2.28.10"
-      "libsoup-2"
-      "libsoup-2.74.3"
-      "libsoup"
-    ];
-  };
+	nixpkgs.config = {
+		allowUnfree = true;
+		allowBroken = true;
+		permittedInsecurePackages = [
+			"mbedtls-2"
+			"mbedtls-2.28.10"
+			"libsoup-2"
+			"libsoup-2.74.3"
+			"libsoup"
+		];
+	};
 
-  environment.systemPackages = [
-    # Dev
-    pkgs.git
-    pkgs.gh
+	environment.systemPackages = [
+		# Dev
+		pkgs.git
+		pkgs.gh
 
-    # Depencencies
-    pkgs.brightnessctl
-    pkgs.playerctl
-    pkgs.wine
-    pkgs.shc
+		# Depencencies
+		pkgs.brightnessctl
+		pkgs.playerctl
+		pkgs.wine
+		pkgs.shc
 
-    # Tui
-    pkgs.lazygit
-    pkgs.yazi
-    pkgs.spotify-player
-    pkgs.fastfetch
-    pkgs.nitch
-    pkgs.cava
-    pkgs.asciiquarium
+		# Tui
+		pkgs.lazygit
+		pkgs.yazi
+		pkgs.spotify-player
+		pkgs.fastfetch
+		pkgs.nitch
+		pkgs.cava
+		pkgs.asciiquarium
 
-    # Gui
-    pkgs.nautilus
-    pkgs.obsidian
-    pkgs.vesktop
-    pkgs.zed-editor
-    pkgs.vscode
-    pkgs.mpv
-  ];
+		# Gui
+		pkgs.nautilus
+		pkgs.obsidian
+		pkgs.vesktop
+		pkgs.zed-editor
+		pkgs.vscode
+		pkgs.mpv
+		pkgs.prismlauncher
+	];
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    noto-fonts
-  ];
+	fonts.packages = with pkgs; [
+		nerd-fonts.jetbrains-mono
+		noto-fonts
+	];
 }
