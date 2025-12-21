ad72d0bce4067d07ef89d2c701c79035f4c67145
amireal98 <amireal98@proton.me>
2025-12-20 19:11:35 -0600

    /configuration.nix is now suckless

--- configuration.nix
+++ configuration.nix
@@ -1,60 +1,60 @@
 { inputs, config, lib, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
+	imports =
+		[
+			./hardware-configuration.nix
 
-      ./modules/nixos/stylix.nix
-      ./modules/nixos/packages.nix
-      ./modules/nixos/programs.nix
-      ./modules/nixos/services.nix
-      ./modules/nixos/audio.nix
-      ./modules/nixos/gaming.nix
+			./modules/nixos/stylix.nix
+			./modules/nixos/packages.nix
+			./modules/nixos/programs.nix
+			./modules/nixos/services.nix
+			./modules/nixos/audio.nix
+			./modules/nixos/gaming.nix
 
-      ./modules/nixos/hyprland.nix
-      ./suckless/suckless.nix
+			./modules/nixos/hyprland.nix
+			./suckless/suckless.nix
 
-      inputs.home-manager.nixosModules.home-manager
-    ];
+			inputs.home-manager.nixosModules.home-manager
+		];
 
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    limine = {
-      enable = true;
-      efiSupport = true;
-    };
-  };
+	boot.loader = {
+		efi.canTouchEfiVariables = true;
+		limine = {
+			enable = true;
+			efiSupport = true;
+		};
+	};
 
-  networking.hostName = "moon";
-  networking.networkmanager.enable = true;
+	networking.hostName = "moon";
+	networking.networkmanager.enable = true;
 
-  time.timeZone = "America/Mexico_City";
-  console = {
-    keyMap = "us";
-  };
+	time.timeZone = "America/Mexico_City";
+	console = {
+		keyMap = "us";
+	};
  
-  services.displayManager.ly.enable = true;
-  services.libinput.enable = true;
+	services.displayManager.ly.enable = true;
+	services.libinput.enable = true;
  
-  users.users.amireal = { # its me
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      tree
-    ];
-  };
+	users.users.amireal = { # its me
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+		packages = with pkgs; [
+			tree
+		];
+	};
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      amireal = import ./home.nix;
-    };
-  };
+	home-manager = {
+		extraSpecialArgs = { inherit inputs; };
+		users = {
+			amireal = import ./home.nix;
+		};
+	};
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
-  system.stateVersion = "25.11"; # change = boom
+	system.stateVersion = "25.11"; # change = boom
 
 }
 
