200865cad5921568bad302ebe1540787c5c81034
amireal98 <amireal98@proton.me>
2025-12-20 19:11:24 -0600

    /home.nix now is suckless

--- home.nix
+++ home.nix
@@ -1,55 +1,55 @@
 { inputs, pkgs, config, ... }:
 
 let
-  username = "amireal";
+	username = "amireal";
 in
 
 {
-  imports = [
-    ./modules/home-manager/dotfiles.nix
+	imports = [
+		./modules/home-manager/dotfiles.nix
 
-    ./suckless/suckless-hm.nix
+		./suckless/suckless-hm.nix
 
-    inputs.zen-browser.homeModules.beta
-  ];
+		inputs.zen-browser.homeModules.beta
+	];
   
-  home = {
-    username = "amireal";
-    homeDirectory = "/home/amireal";
-    stateVersion = "25.11";
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
-  };
+	home = {
+		username = "amireal";
+		homeDirectory = "/home/amireal";
+		stateVersion = "25.11";
+		sessionVariables = {
+			EDITOR = "nvim";
+		};
+	};
 
-  programs.home-manager.enable = true;
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      nrs = "sudo nixos-rebuild switch --flake ~/Nixos#moon";
-      l = "ls -l";
-      n = "clear && nvim";
-      yz = "yazi && clear";
-    };
-  };
-  programs.zen-browser = {
-    enable = true;
-    profiles = {
-      ${username} = {
-	id = 0;
-	name = "${username}.default";
-	isDefault = true;
-      };
-    };
-  };
+	programs.home-manager.enable = true;
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			nrs = "sudo nixos-rebuild switch --flake ~/Nixos#moon";
+			l = "ls -l";
+			n = "clear && nvim";
+			yz = "yazi && clear";
+		};
+	};
+	programs.zen-browser = {
+		enable = true;
+		profiles = {
+			${username} = {
+				id = 0;
+				name = "${username}.default";
+				isDefault = true;
+			};
+		};
+	};
 
-  home.packages = [
-    pkgs.neovim
-    pkgs.ripgrep
-    pkgs.nil
-    pkgs.nixpkgs-fmt
-    pkgs.nodejs
-    pkgs.gcc
-  ];
+	home.packages = [
+		pkgs.neovim
+		pkgs.ripgrep
+		pkgs.nil
+		pkgs.nixpkgs-fmt
+		pkgs.nodejs
+		pkgs.gcc
+	];
 
 }
