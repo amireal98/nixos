0dc69c7854a5af6e3d26729dc77fc59f7ca17c0b
amireal98 <amireal98@proton.me>
2025-12-21 13:55:33 -0600

    same as anterior commit

--- flake.nix
+++ flake.nix
@@ -44,10 +44,10 @@
 				./configuration.nix
 				stylix.nixosModules.stylix
 				{
-					stylix.homeManagerIntegration.autoImport = false;
 					home-manager.useGlobalPkgs = true;
 					home-manager.useUserPackages = true;
 					home-manager.backupFileExtension = "bak";
+					stylix.homeManagerIntegration.autoImport = false;
 				}
 			];
 		};
