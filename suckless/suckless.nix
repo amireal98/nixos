d4040230af1bfad789002267e18cfdbf49794182
amireal98 <amireal98@proton.me>
2025-12-20 18:22:38 -0600

    formated some files to be like suckless code

--- suckless/suckless.nix
+++ suckless/suckless.nix
@@ -1,16 +1,15 @@
 { inputs, config, lib, pkgs, ... }:
 
 {
-  services.xserver = {
-    enable = true;
-    autoRepeatDelay = 200;
-    autoRepeatInterval = 35;
-    windowManager.dwm = {
-      enable = true;
-      package = pkgs.dwm.overrideAttrs {
-	src = ./dwm;
-      };
-    };
-  };
-
+	  services.xserver = {
+		    enable = true;
+		    autoRepeatDelay = 200;
+		    autoRepeatInterval = 35;
+		    windowManager.dwm = {
+			      enable = true;
+			      package = pkgs.dwm.overrideAttrs {
+					src = ./dwm;
+			      };
+		    };
+	  };
 }
