e7ea112f73903c854b0a3a21169f78c3ae57ece2
amireal98 <amireal98@proton.me>
2025-12-24 15:17:30 -0600

    removed suckless-hm.nix module

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@ in
 		./modules/home-manager/dotfiles.nix
 		# ./modules/home-manager/stylix.nix
 
-		./suckless/suckless-hm.nix
+		# ./suckless/suckless-hm.nix
 
 		inputs.zen-browser.homeModules.beta
 		# inputs.stylix.homeModules.stylix
