d0a4bc490dc098de554b7ad3577e8539b594854d
amireal98 <amireal98@proton.me>
2025-12-25 18:19:52 -0600

    added lazygit config to snacks.nvim

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -11,6 +11,26 @@ return {
 
 			lazygit = {
 				enabled = true,
+				configure = true,
+				config = {
+					os = { editPreset = "nvim-remote" },
+					gui = {
+						nerdFontsVersion = "3",
+					},
+				},
+				win = { style = "Lazygit" },
+				theme = {
+					[241]                      = { fg = "Special" },
+					activeBorderColor          = { fg = "MatchParen", bold = true },
+					cherryPickedCommitBgColor  = { fg = "Identifier" },
+					cherryPickedCommitFgColor  = { fg = "Function" },
+					defaultFgColor             = { fg = "Normal" },
+					inactiveBorderColor        = { fg = "FloatBorder" },
+					optionsTextColor           = { fg = "Function" },
+					searchingActiveBorderColor = { fg = "MatchParen", bold = true },
+					selectedLineBgColor        = { bg = "Visual" }, -- set to `default` to have no background colour
+					unstagedChangesColor       = { fg = "DiagnosticError" },
+				},
 			},
 		},
 
