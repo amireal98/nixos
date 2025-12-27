f358b4deb7760bbd325af81f6da343dd2c897ea1
amireal98 <amireal98@proton.me>
2025-12-27 17:35:28 -0600

    i missed a "s"

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -37,7 +37,7 @@
 			(pkgs.iosevka-bin.override { variant = "Etoile"; })
 			(pkgs.iosevka-bin.override { variant = "Aile"; })
 	];
-	fontconfig = {
+	fontsconfig = {
 		enable = true;
 		antialias = true;
 		hinting.enable = true;
