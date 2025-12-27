7c2f33d0455474951fda6dc0b9fb9af1775f3951
amireal98 <amireal98@proton.me>
2025-12-26 18:06:34 -0600

    changed lualine.nvim config

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,23 +1,33 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require('lualine').setup {
 				options = {
-					disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+					disabled_filetypes = { statusline = { "dashboard", "snacks_dashboard" } },
+					icons_enabled = false,
 					theme = "auto",
-					section_separators = '',
-					component_separators = '',
-					icons_enabled = true,
+					section_separators = "",
+					component_separators = "",
 				},
-				dependencies = { "nvim-tree/nvim-web-devicons" },
 				sections = {
-					lualine_a = { 'mode' },
-					lualine_b = { 'branch', 'diff', 'diagnostics' },
-					lualine_c = { 'filename' },
-					lualine_x = { 'filetype' },
-					lualine_y = { 'progress' },
-					lualine_z = { 'location' },
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = { "filename" },
+					lualine_x = {
+						function()
+							local encoding = vim.o.fileencoding
+							if encoding == "" then
+								return vim.bo.fileformat .. " :: " .. vim.bo.filetype
+							else
+								return encoding .. " :: " .. vim.bo.fileformat .. " :: " .. vim.bo.filetype
+							end
+						end,
+
+					},
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
 				},
 			}
 		end,
