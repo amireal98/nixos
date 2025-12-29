9fe4f7ea65758e8af1f0c46b73ef629218d4ef0b
amireal98 <amireal98@proton.me>
2025-12-28 18:23:11 -0600

    minor changes

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,14 +1,19 @@
+-- general settings or something
 vim.opt.title = true
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.showmode = false
 
+vim.o.scrolloff = 5
+
+-- visual things?
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 
+-- encondig
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
